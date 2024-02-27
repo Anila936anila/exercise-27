c837170c2141e90228e4d79191fffb18368fbba5
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:58:54 -0800

    Alien color
    Typescript 45Q assignment exercise 27

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+// version 1:1st version where alien color is green
+console.log(`1st version where alien color is green`);
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log('congratulation! You just earned 5 points.');
+}
+else if (alien_color === 'yellow') {
+    console.log('congratulation! You just earned 10 points.');
+}
+else if (alien_color === 'red') {
+    console.log('congratulation! You just earned 15 points.');
+}
+else {
+    console.log('Unknown alien color');
+}
+//Version 2:2nd version where alien color is yellow
+console.log(`2nd version where align color is yellow`);
+alien_color: 'yellow';
+if (alien_color === 'green') {
+    console.log('congratulation! You just earned 5 points.');
+}
+else if (alien_color === 'yellow') {
+    console.log('congratulation! You just earned 10 points.');
+}
+else if (alien_color === 'red') {
+    console.log('congratulation! You just earned 15 points.');
+}
+else {
+    console.log('Unknown alien color');
+}
+//3rd version where alien coior is red
+console.log(`3rd version where align color is red`);
+alien_color: 'red';
+if (alien_color === 'green') {
+    console.log('congratulation! You just earned 5 points.');
+}
+else if (alien_color === 'yellow') {
+    console.log('congratulation! You just earned 10 points.');
+}
+else if (alien_color === 'red') {
+    console.log('congratulation! You just earned 15 points.');
+}
+else {
+    console.log('Unknown alien color');
+}
